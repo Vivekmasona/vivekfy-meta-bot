4150443ad02f3c2941b3b89d446a422d85744696
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2025-02-13 15:55:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ async function processAudioWithWatermark(audioUrl, coverUrl, title, artist, chat
                     '-map', '0:a',
                     '-map', '2:v',
                     '-c:v', 'mjpeg',
-                    '-vf', "drawtext=text='VIVEKFY-AI':fontcolor=#000000:fontsize=60:box=1:boxcolor=#ffffff@0.9:x=(W-text_w)/2:y=H*0.8-text_h"
+                    '-vf', "drawtext=text='vivekfy ai':fontcolor=#000000:fontsize=60:box=1:boxcolor=#ffffff@0.9:x=(W-text_w)/2:y=H*0.8-text_h"
                 ])
                 .save(finalOutputName)
                 .on('end', async () => {
