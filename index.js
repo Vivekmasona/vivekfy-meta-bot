6bd9d34552114dcac523104f62267e639c3677f3
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2025-02-17 11:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Bot Token
 const botToken = '7426827982:AAFNLzurDSYX8rEmdI-JxCRyKoZMtszTL7I';
-const youtubeApiKey = 'AIzaSyBX_-obwbQ3MZKeMTYS9x8SzjiXojl3nWs';
+const youtubeApiKey = 'AIzaSyBfsNcJJHd-O0ftUzH2KqIRc_KhXgPXne0';
 
 // API URLs
 const koyebApiAudio = 'https://thirsty-editha-vivekfy-6cef7b64.koyeb.app/play?url=';
@@ -221,5 +221,4 @@ function keepAlive() {
 }
 
 keepAlive();
-
 
